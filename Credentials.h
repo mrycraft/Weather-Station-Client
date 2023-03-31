43dfe71f5dc4b1d54357d61dba356c8cc89af162
Nathan George <kosmosisdire@gmail.com>
2023-03-31 11:38:26 -0400

    re-add Credentials.h

---
+++ Credentials.h
@@ -0,0 +1,18 @@
+// This file contains the credentials for the Wifi client. 
+
+// DO NOT COMMIT THIS FILE TO GIT ONCE YOU HAVE SET IT UP.
+
+// WiFi SSID
+#define SSID "SSID Here"
+
+// WiFi Password
+#define PASSWORD "WiFi Password Here"
+
+// Server Hostname or IP Address
+#define HOST_NAME "nathang.dev"
+
+// Server Port
+#define PORT 0000
+
+// Device name used to uniquely identify this device
+#define DEVICE_NAME "Device Name Here"
